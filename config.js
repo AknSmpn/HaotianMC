1ca14a047ed3e2335133fd5df1498dc071b355c8
Cestaa <akunsimpen2007@gmail.com>
2026-01-04 17:05:51 +0700

    Fix serverLogo filename case

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     // 1. INFO SERVIDOR
     serverName: "HaotianMC",
     serverIp: "basic-6.alstore.space:25575",
-    serverLogo: "logo.png", // Asegúrate de que existe
+    serverLogo: "Logo.png", // Asegúrate de que existe
     // El favicon se carga automáticamente si se llama "favicon.png"
 
     // 2. REDES SOCIALES
